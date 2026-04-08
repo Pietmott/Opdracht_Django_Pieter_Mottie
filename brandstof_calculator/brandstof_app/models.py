967b6857efbc2e7d44a1225df4eaf9b2e6be46a3
Pietmott <pieter.mottie@student.odisee.be>
2026-04-08 18:26:15 +0200

    nieuwe commit

--- brandstof_calculator/brandstof_app/models.py
+++ brandstof_calculator/brandstof_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BrandstofBerekening(models.Model):
+    afstand = models.FloatField()
+    verbruik = models.FloatField()
+    prijs_per_liter = models.FloatField()
+    liters = models.FloatField()
+    kost = models.FloatField()
+
+    def __str__(self):
+        return f"{self.afstand} km - €{self.kost}"
